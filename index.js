461df6ad356d0982cb682aacda0b96e8f43aa990
Luca Bergese <luca.bergese@infosys.it>
2022-10-09 16:45:42 +0200

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'hi there' });
+    res.send({ ciao: 'Francescoh' });
 });
 
 const PORT = process.env.PORT || 5000; // per Hiroku sula prima in locale usa 5000
