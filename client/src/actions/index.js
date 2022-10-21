8c15b1cad8cf3d6cee2a48afd440e807b40b29c6
Luca Bergese <lucabergese@Air-di-Luca.station>
2022-10-21 16:52:45 +0200

    added billing and client setup

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,5 +3,10 @@ import { FETCH_USER } from './types';
 
 export const fetchUser = () => async dispatch => {
     const res = await axios.get('/api/current_user');
-    dispatch ({ type: FETCH_USER, payload: res.data}); //ci serve solo il data che contine l'id google
+    dispatch ({ type: FETCH_USER, payload: res.data}); //action per recuperare user id ci serve solo il data che contine l'id google
 };
+
+export const handleToken = (token) => async dispatch => {
+    const res = await axios.post('/api/stripe',token);
+    dispatch({ type: FETCH_USER, payload: res.data}); //action per passare il token di ritorno di stripe
+}
--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import StripeCheckOut from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+import * as action from '../actions';
 
 class Payments extends Component {
     render() {
-        debugger;
         return(<StripeCheckOut
+            name="Emaily"
+            description='$5 for 5 emails'
             amount={500} //scrivere solo 5 corrisponde a 5 cent, aggiungere due zeri per fare 5 dollari
-            token={token => console.log(token)} // callback di autorizzazione da stripe
+            token={token => this.props.handleToken(token)} // callback di autorizzazione da stripe
             stripeKey={process.env.REACT_APP_STRIPE_KEY} // PASSO LA CHIAVE PUBBLICA DI STRIPE   
-        />
+        >
+            <button className='btn'>
+                <b>Add Credits</b>
+            </button>
+        </StripeCheckOut>
         ); //inserisco il modulo di stripe checkout per farlo vedere in page
     }
 }
 
-export default Payments;
+export default connect(null, action) (Payments);
